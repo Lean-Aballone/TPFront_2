95d19994e99e94de46929abc61787c16b776f06b
Lean-Aballone <l.aballone@gmail.com>
2025-10-15 18:53:23 -0300

    Added: Layout component, changed (css): App.css

--- src/pages/Layout.jsx
+++ src/pages/Layout.jsx
@@ -1,11 +1,15 @@
-import { Link, Outlet } from "react-router-dom";
-import Sidebar from "../components/Sidebar"
+import { Outlet } from "react-router-dom";
+import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
+
 
 const Layout = () => {
-    return <div id="Layout-container">
-        <Sidebar></Sidebar>
-        <Outlet />
- </div>;
-}
+  return (
+    <div id="Layout-container">
+      <Sidebar />
+      <Outlet />
+    </div>
+  );
+};
 
 export default Layout;
