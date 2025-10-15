83e970ff5c23c1f451303f6c5854d2b15cbae0b7
Lean-Aballone <l.aballone@gmail.com>
2025-10-15 18:54:44 -0300

    Created: Bitacora

--- src/pages/Bitacora.jsx
+++ src/pages/Bitacora.jsx
@@ -1,10 +1,28 @@
+import Footer from "../components/Footer";
+import '../Bitacora/bitacora.css'
+import Article from "../Bitacora/Article";
+import reunionesData from '../Bitacora/reuniones.json'
+
 const Bitacora = () => {
     return (
-        <div>
-            <h1>
-                Bitácora
-            </h1>
-        </div>
+        <main id="app" className="page-content bitacoraBackground">
+            <section className="container section bitacoraStyle">
+                <h2>
+                    Bitácora
+                </h2>
+                <section id="sectionBitacora">
+                {reunionesData.map(reunion => (
+                    <Article
+                    key={reunion.id}
+                    titulo={reunion.titulo}
+                    fecha={reunion.fecha}
+                    info={reunion.info}
+                    />
+                ))}
+                </section>
+            </section>
+            <Footer/>
+        </main>
     );
 }
 
