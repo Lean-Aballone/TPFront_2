75874c155ff55a566ca848cc5cb2c8a67a5ef9d6
Lean-Aballone <l.aballone@gmail.com>
2025-10-15 23:04:24 -0300

    Created: Integrantes component

---
+++ src/portada/Integrantes.jsx
@@ -0,0 +1,19 @@
+import Card from "./Card";
+
+const Integrantes = () => {
+    return (
+        <section className="container section flex">
+            <h2>Integrantes grupo 14</h2>
+            <p>Conocé al equipo del grupo 14. Cada tarjeta redirige a su página con más detalles.</p>
+            <div className="cardSection">
+                <p>Elegí un perfil para ver su tarjeta.</p>
+                <Card img={"src/assets/img/chikis.jpg"} nombre={"Erika"} frase={"Me gusta programar, aprender en equipo y disfrutar de mis cuatro perros."}/>
+                <Card img={"src/assets/img/gonzalo-profile.jpg"} nombre={"Gonza"} frase={"Curioso por la tecnología, creativo y con ganas de seguir evolucionando como profesional."}/>
+                <Card img={"src/assets/img/kity.jpeg"} nombre={"Mariano"} frase={"Apasionado por la tecnología y la innovación, con interés en proyectos de impacto real"}/>
+                <Card img={"src/assets/img/carlina.png"} nombre={"Lean"} frase={"Desarrollador en crecimiento, siempre aprendiendo y compartiendo experiencias."}/>
+            </div>
+        </section>
+    );
+}
+
+export default Integrantes;
