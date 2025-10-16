96858a6714f99ae555d2537197c5862869ad2043
Lean-Aballone <l.aballone@gmail.com>
2025-10-15 23:04:52 -0300

    Portada: Added components + styles

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,17 @@
 import Footer from "../components/Footer";
+import Welcome from "../portada/Welcome";
+import Integrantes from "../portada/Integrantes";
+import "../portada/portada.css";
 const Home = () => {
     return (
-        <div>
-            <h1>
-                Home
-            </h1>
+        <div className="flex">
+            <Welcome 
+                titulo={"Desarrollo de sistemas web (front End)"} 
+                subtitulo={"Instituto de Formación Técnica Superior N° 29"}
+                grupo={"Grupo 14"}
+                bienvenida={"Bienvenidos a nuestra segunda entrega del trabajo práctico para Desarrollo Web. En esta ocasión migramos el trabajo práctico anterior a React + Vite. Los invitamos a ver cómo las iremos implementando a lo largo de las entregas."} 
+            />
+            <Integrantes/>
             <Footer/>
         </div>
     );
