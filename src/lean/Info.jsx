5d424ba6c6f1de9bff375033fd15558a0da3c089
Lean-Aballone <l.aballone@gmail.com>
2025-10-18 02:11:45 -0300

    Info.jsx Added: Ubicación, Formación, Edad, Mascota

--- src/lean/Info.jsx
+++ src/lean/Info.jsx
@@ -1,12 +1,12 @@
 export default function Info({ nombre, ubicacion, formacion, edad, mascota }){
 
     return (
-        <article>
+        <article className="dark-bg">
             <h2>{ nombre }</h2>
-            <p>{ ubicacion }</p>
-            <p>{ formacion }</p>
-            <p>{ edad }</p>
-            <p> { mascota } </p>
+            <p><b>Ubicación actual: </b>{ ubicacion }</p>
+            <p><b>Formación: </b>{ formacion }</p>
+            <p><b>Edad: </b>{ edad }</p>
+            <p><b>Mascota: </b> { mascota } </p>
         </article>
     );
 }
