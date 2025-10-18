dd78b86b06c6c453c34ea31d07133390ee98ac4d
Lean-Aballone <l.aballone@gmail.com>
2025-10-18 02:13:55 -0300

    Peliculas.jsx: Added: backgroundImage prop 'bg'

--- src/lean/Peliculas.jsx
+++ src/lean/Peliculas.jsx
@@ -1,9 +1,9 @@
-export default function Peliculas({name, img, link1, link2, info}) {
+export default function Peliculas({name, bg, img, link1, link2, info}) {
     return (
-        <article>
+        <article style={{textAlign:"justify", backgroundImage:bg}}>
             <div className="blur white-bg">
                 <a href={link1} target="_blank">
-                    <div /*class="white-bg black-text flex"*/>
+                    <div>
                       <h4>{name}</h4>
                       <img itemProp="image" width="160" height="240" src={img} alt={name} />
                     </div>
