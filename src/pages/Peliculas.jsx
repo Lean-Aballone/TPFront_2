cb1da77a3c6b345f08f4ca2d42beeb6bd6eac50c
Lean-Aballone <l.aballone@gmail.com>
2025-10-19 19:55:07 -0300

    Added: Page pelicula.jsx

---
+++ src/pages/Peliculas.jsx
@@ -0,0 +1,29 @@
+import React, { useEffect, useState } from "react";
+import MovieCard from "../components/MovieCard.jsx";
+import Footer from "../components/Footer.jsx"
+import "../seccionPeliculas/peliculas.css";
+
+export default function App() {
+  const [peliculas, setPeliculas] = useState([]);
+
+  useEffect(() => {
+    fetch("src/seccionPeliculas/data.json")
+      .then((res) => res.json())
+      .then((data) => setPeliculas(data))
+      .catch((err) => console.error("Error al cargar JSON:", err));
+  }, []);
+
+  return (
+    <>
+        <div className="app">
+        <h1 className="white-bg sideMargin">Listado de Películas:</h1>
+        <div className="grid white-bg sideMargin">
+            {peliculas.map((peli) => (
+            <MovieCard key={peli.id} {...peli} />
+            ))}
+        </div>
+        <Footer />
+        </div>
+    </>
+  );
+}
